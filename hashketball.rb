e915fc1b408e81c51433a8f441c8a667e0f1b853
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 23:29:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -140,7 +140,7 @@ end
 def shoe_size (name)
   game_hash.each do |team, team_data|
     team_data[:players].each do |player|
-      if player[:player_name] == player_search
+      if player[:player_name] == name
         return player[:shoe]
       end
     end
