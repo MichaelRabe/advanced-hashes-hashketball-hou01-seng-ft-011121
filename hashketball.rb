ad9932e18cefb904aa15cf576b578f25a4bc2200
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 23:02:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -128,6 +128,6 @@ def game_hash
 end
 
 def num_points_scored (player_name)
-  players = game_hash[:home][:players].merge(game_hash[:away][:players])
+  players = game_hash[:home][:players] + (game_hash[:away][:players])
   binding.pry
 end  
