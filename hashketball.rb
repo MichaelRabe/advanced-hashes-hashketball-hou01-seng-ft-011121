2b5f6931b9396f0628ffc4d00bb8fdcc6d073842
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 22:46:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -62,7 +62,7 @@ def game_hash
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
       players: {
-      "Jeff Adrien" =>
+      "Jeff Adrien" => {
           number: 4,
           shoe: 18,
           points: 10,
@@ -72,7 +72,7 @@ def game_hash
           blocks: 7,
           slam_dunks: 2
         },
-      "Bismack Biyombo" =>
+      "Bismack Biyombo" => {
           number: 0,
           shoe: 16,
           points: 12,
@@ -82,7 +82,7 @@ def game_hash
           blocks: 15,
           slam_dunks: 10
         },
-        "DeSagna Diop" =>
+        "DeSagna Diop" => {
           number: 2,
           shoe: 14,
           points: 24,
@@ -92,7 +92,7 @@ def game_hash
           blocks: 5,
           slam_dunks: 5
         },
-        "Ben Gordon" =>
+        "Ben Gordon" => {
           number: 8,
           shoe: 15,
           points: 33,
@@ -102,7 +102,7 @@ def game_hash
           blocks: 1,
           slam_dunks: 0
         },
-        "Kemba Walker" =>
+        "Kemba Walker" => {
           number: 33,
           shoe: 15,
           points: 6,
