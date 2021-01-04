6be78bfc4a14aef5a304e28d56ad07e134374f0d
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 23:06:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
       colors: ["Black", "White"],
       players: [
         {
-          player_name: "Alan Anderson",
+          player_name=> "Alan Anderson",
           number: 0,
           shoe: 16,
           points: 22,
@@ -18,7 +18,7 @@ def game_hash
           slam_dunks: 1
         },
         {
-          player_name: "Reggie Evans",
+          player_name=> "Reggie Evans",
           number: 30,
           shoe: 14,
           points: 12,
@@ -29,7 +29,7 @@ def game_hash
           slam_dunks: 7
         },
         {
-          player_name: "Brook Lopez",
+          player_name=> "Brook Lopez",
           number: 11,
           shoe: 17,
           points: 17,
@@ -40,7 +40,7 @@ def game_hash
           slam_dunks: 15
         },
         {
-          player_name: "Mason Plumlee",
+          player_name=> "Mason Plumlee",
           number: 1,
           shoe: 19,
           points: 26,
@@ -51,7 +51,7 @@ def game_hash
           slam_dunks: 5
         },
         {
-          player_name: "Jason Terry",
+          player_name=> "Jason Terry",
           number: 31,
           shoe: 15,
           points: 19,
@@ -68,7 +68,7 @@ def game_hash
       colors: ["Turquoise", "Purple"],
       players: [
         {
-          player_name: "Jeff Adrien",
+          player_name=> "Jeff Adrien",
           number: 4,
           shoe: 18,
           points: 10,
@@ -79,7 +79,7 @@ def game_hash
           slam_dunks: 2
         },
         {
-          player_name: "Bismack Biyombo",
+          player_name=> "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
@@ -90,7 +90,7 @@ def game_hash
           slam_dunks: 10
         },
         {
-          player_name: "DeSagna Diop",
+          player_name=> "DeSagna Diop",
           number: 2,
           shoe: 14,
           points: 24,
@@ -101,7 +101,7 @@ def game_hash
           slam_dunks: 5
         },
         {
-          player_name: "Ben Gordon",
+          player_name=> "Ben Gordon",
           number: 8,
           shoe: 15,
           points: 33,
